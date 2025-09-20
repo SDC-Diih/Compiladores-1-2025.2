ae590253b2d1167eca3ac41ffeb400c3a6458dd1
Arthur Alves de Matos <git.arthurmatos@gmail.com>
2025-09-20 15:39:14 -0300

    feat: implementar comentários
    Co-Authored-By: Diogo <48413982+SDC-Diih@users.noreply.github.com>
    Co-Authored-By: Alexandre Beck <82113486+zzzBECK@users.noreply.github.com>

---
+++ testeCases/teste2.c
@@ -0,0 +1,14 @@
+// Comentario de uma linha
+
+/*
+    Nada aqui é pra quebrar, tudo deve ser ignorado
+    int v;
+    v = 0;
+    print v;
+*/
+
+int a;
+a = 1;
+print a;
+
+// Sucesso ao printar int a; print a; sem quebrar

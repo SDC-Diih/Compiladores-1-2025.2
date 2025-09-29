4d17649da6c6bf426cd58aad22d0d45ef9f23ce6
pedroMADBR <trabalhos.pedromiguel@gmail.com>
2025-09-29 09:32:40 -0300

    Alteração da main para visualizar AST

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include "../ast/ast.h"
+
+extern ASTNode *ast_root;
 
 int yyparse();
 
 int main() {
+
+    printf(" Interpretador em C (Modo AST) \n");
+    printf(" Insira o código abaixo e pressione Ctrl+D (Linux/Mac) ou Ctrl+Z (Windows) para finalizar a entrada: \n");
+
     yyparse();
+
+    if (ast_root) {
+        printf("Árvore Sintática Abstrata (AST): \n");
+        print_ast(ast_root, 0);
+        free_ast(ast_root);
+    } else {
+        printf(" Falha ao gerar AST! \n");
+    }
     return 0;
 }

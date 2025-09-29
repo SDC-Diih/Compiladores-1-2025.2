fc24aa74fa613c5b7ba2a1bf5931eee7dc8afd10
pedroMADBR <trabalhos.pedromiguel@gmail.com>
2025-09-29 10:20:31 -0300

    Ajuste de visualização da AST na main.c

--- src/main.c
+++ src/main.c
@@ -6,13 +6,15 @@ extern ASTNode *ast_root;
 int yyparse();
 
 int main() {
-
+    printf("  \n");
     printf(" Interpretador em C (Modo AST) \n");
     printf(" Insira o código abaixo e pressione Ctrl+D (Linux/Mac) ou Ctrl+Z (Windows) para finalizar a entrada: \n");
+    printf(" -------------------------------------------------- \n");
 
     yyparse();
 
     if (ast_root) {
+        printf(" -------------------------------------------------- \n");
         printf("Árvore Sintática Abstrata (AST): \n");
         print_ast(ast_root, 0);
         free_ast(ast_root);
